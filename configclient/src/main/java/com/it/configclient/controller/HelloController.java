dc0f90d170cdbec8383995dc971d89d2b73a3fb8
545009020@qq.com <ziji770880>
2020-12-31 11:45:28 +0800

    修改配置文件

---
+++ configclient/src/main/java/com/it/configclient/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.it.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author WangYerou
+ * @Date 2020/12/31 11:30
+ */
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @Value("${server.port}")
+    private String port;
+
+    @GetMapping("/index")
+    public String index(){
+        return this.port;
+    }
+}
